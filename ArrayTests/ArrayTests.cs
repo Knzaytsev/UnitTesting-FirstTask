ed915d51eb2c260b710e98612c63d7b32add753a
adugeen <konstantzts@gmail.com>
2019-09-11 16:09:16 +0500

    Добавление проекта с тестами

---
+++ ArrayTests/ArrayTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayClassLibraryTests;
+
+namespace ArrayClassLibraryTests
+{
+    [TestClass]
+    public class ArrayTests
+    {
+        [TestMethod]
+        public void FormingArrayTest()
+        {
+            string input = "1.2, 2, 3.5";
+            double[] expectedArray = new double[] { 1.2, 2, 3.5 };
+            WorkArray workArray = new WorkArray();
+        }
+    }
+}
---
+++ ArrayTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArrayTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArrayTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df17b910-2693-4dcb-9ce7-1dec3930c116")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
