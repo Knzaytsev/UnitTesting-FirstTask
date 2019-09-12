e7774a1beec156fba42a2941038f7104826316d5
adugeen <konstantzts@gmail.com>
2019-09-12 11:27:34 +0500

    Добавление новых тестов

--- ArrayTests/ArrayTests.cs
+++ ArrayTests/ArrayTests.cs
@@ -9,10 +9,21 @@ namespace ArrayClassLibraryTests
         [TestMethod]
         public void FormingArrayTest()
         {
-            string input = "1.2, 2, 3.5";
+            int countTest = 2;
+            string[] testStrings = new string[] { "1.2, 2, 3.5", "-1", "" };
+            /*string input = "1.2, 2, 3.5";
             double[] expectedArray = new double[] { 1.2, 2, 3.5 };
-            double[] result = WorkArray.FormingArray(input);
-            CollectionAssert.AreEqual(expectedArray, result);
+            double[] result = WorkArray.FormingArray(input);*/
+            double[][] result = new double[countTest][];
+            double[][] expectedArrays = new double[][] { new double[] { 1.2, 2, 3.5 }, new double[] { -1 }, new double[] { } };
+            for (int i = 0; i < countTest; ++i)
+            {
+                result[i] = WorkArray.FormingArray(testStrings[i]);
+            }
+            for (int i = 0; i < countTest; ++i)
+            {
+                CollectionAssert.AreEqual(expectedArrays[i], result[i]);
+            }
         }
 
         [TestMethod]
@@ -23,6 +34,32 @@ namespace ArrayClassLibraryTests
             double[] expectedArray = new double[] { 2, 3, 5 };
             double[] result = WorkArray.Remove(element, inputArray);
             CollectionAssert.AreEqual(expectedArray, result);
+        }
+
+        [TestMethod]
+        public void EquationTest()
+        {
+            int countTest = 2;
+            double[][] inputValues = new double[][] { new double[] { 3, -14, -5 }, new double[] { 0, 0, 0 } };
+            double[][] expectedValues = new double[][] { new double[] { 5, (double)-1 / 3 }, new double[] { } };
+            double[][] result = new double[countTest][];
+            for (int i = 0; i < countTest; ++i)
+            {
+                double a = inputValues[i][0];
+                double b = inputValues[i][1];
+                double c = inputValues[i][2];
+                MathEquation equation = new MathEquation(a, b, c);
+                result[i] = equation.SolveEquation();
+            }
+            for (int i = 0; i < countTest; ++i)
+            {
+                CollectionAssert.AreEqual(expectedValues[i], result[i]);
+            }
+            /*double a = 3, b = -14, c = -5;
+            double[] expected = new double[] { 5, (double)-1/3 };
+            MathEquation equation = new MathEquation(a, b, c);
+            double[] result = equation.SolveEquation();
+            CollectionAssert.AreEqual(expected, result);*/
         }
     }
 }
---
+++ UnitTesting-FirstTask/MathEquation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting_FirstTask
+{
+    public class MathEquation
+    {
+        private double a, b, c;
+        public MathEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double[] SolveEquation()
+        {
+            if (a != 0)
+                return SolveEquationDiscriminant();
+            else if (b != 0)
+                return SolveEquationLinear();
+            return new double[] { };
+        }
+
+        private double[] SolveEquationLinear()
+        {
+            return new double[] { -c / b };
+        }
+
+        private double[] SolveEquationDiscriminant()
+        {
+            List<double> result = new List<double>();
+            double d = b * b - 4 * a * c;
+            double x1 = 0;
+            double x2 = 0;
+            if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                result.Add(x1);
+                result.Add(x2);
+            }
+            else if (d == 0)
+            {
+                x1 = -b / (2 * a);
+                result.Add(x1);
+            }
+            return result.ToArray();
+        }
+    }
+}
